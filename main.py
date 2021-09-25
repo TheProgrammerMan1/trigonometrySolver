4304c50cf14d0262832544dc15c8f140b0617f8c
Gabriel Teles <75281810+TheProgrammerMan1@users.noreply.github.com>
2021-09-25 13:47:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 import math
 
 def trigonometria():
-    sen30 = 0.5
-    sen45 = 0.7071
-    sen60 = 0.8660
-    cos30 = 0.8660
-    cos45 = 0.7071
-    cos60 = 0.5
-    tan30 = 0.5774
-    tan45 = 1
-    tan60 = 1.7321
+    valores = [
+        {'sen30': 0.5}, {'sen45': 0.7071}, {'sen60': 0.8660}, {'cos30': 0.8660}, {'cos45': 0.7071},
+        {'cos60': 0.5}, {'tan30': 0.5774}, {'tan45': 1}, {'tan60': 1.7321}
+    ]
 
     a = int(input('Digite o valor de a: '))
     co = int(input('Digite o valor de co: '))
@@ -17,19 +12,19 @@ def trigonometria():
 
     pergunta = input('\nDigite o que voce quer encontrar \n cateto oposto (co) \n cateto adjascente(ca) \n hipotenusa (a) \n \n ')
 
-    # CATETO OPOSTO
+        # CATETO OPOSTO
 
     if pergunta == 'co':
         if a > 0:
             pergunta1 = input('angulo formado? ')
             if pergunta1 == '30':
-                co = sen30 * a
+                co = valores[0]['sen30'] * a
                 print(f'O valor do cateto oposto é {co:.2f}')
             elif pergunta1 == '45':
-                co = sen45 * a
+                co = valores[1]['sen45'] * a
                 print(f'O valor do cateto oposto é {co:.2f}')
             elif pergunta1 == '60':
-                co = sen60 * a
+                co = valores[2]['sen60'] * a
                 print(f'O valor do cateto oposto é {co:.2f}')
 
         # TANGENTE PARA CATETO OPOSTO
@@ -37,29 +32,29 @@ def trigonometria():
         elif a == 0:
             pergunta1 = input('angulo formado? ')
             if pergunta1 == '30':
-                co = tan30 * ca
+                co = valores[6]['tan30'] * ca
                 print(f'O valor do cateto oposto é {co:.2f}')
             elif pergunta1 == '45':
-                co = tan45 * ca
+                co = valores[7]['tan45'] * ca
                 print(f'O valor do cateto oposto é {co:.2f}')
             elif pergunta1 == '60':
-                co = tan60 * ca
+                co = valores[8]['tan60'] * ca
                 print(f'O valor do cateto oposto é {co:.2f}')
 
     
-    # CATETO ADJASCENTE
+        # CATETO ADJASCENTE
 
     elif pergunta == 'ca':
         if a > 0:
             pergunta1 = input('angulo formado? ')
             if pergunta1 == '30':
-                ca = cos30 * a
+                ca = valores[3]['cos30'] * a
                 print(f'O valor do cateto adjascente é {ca:.2f}')
             elif pergunta1 == '45':
-                ca = cos45 * a
+                ca = valores[4]['cos45'] * a
                 print(f'O valor do cateto adjascente é {ca:.2f}')
             elif pergunta1 == '60':
-                ca == cos60 * a
+                ca == valores[5]['cos60'] * a
                 print(f'O valor do cateto adjascente é {ca:.2f}')
 
         # TANGENTE PARA CATETO ADJASCENTE
@@ -67,39 +62,39 @@ def trigonometria():
         elif a == 0:
             pergunta1 = input('angulo formado? ')
             if pergunta1 == '30':
-                ca = co / tan30
+                ca = co / valores[6]['tan30']
                 print(f'O valor do cateto oposto é {ca:.2f}')
             elif pergunta1 == '45':
-                ca = co / tan45
+                ca = co / valores[7]['tan45']
                 print(f'O valor do cateto oposto é {ca:.2f}')
             elif pergunta1 == '60':
-                ca = co / tan60
+                ca = co / valores[8]['tan60']
                 print(f'O valor do cateto oposto é {ca:.2f}')
             
-    # HIPOTENUSA
+        # HIPOTENUSA
 
     elif pergunta == 'a':
         if ca == 0:
             pergunta1 = input('angulo formado? ')
             if pergunta1 == '30':
-                a = co / sen30
+                a = co / valores[0]['sen30']
                 print(f'O valor da hipotenusa é {a:.2f}')
             elif pergunta1 == '45':
-                a = co / sen45
+                a = co / valores[1]['sen45']
                 print(f'O valor da hipotenusa é {a:.2f}')
             elif pergunta1 == '60':
-                a = co / sen60
+                a = co / valores[2]['sen60']
                 print(f'O valor da hipotenusa é {a:.2f}')
         if co == 0:
             pergunta1 = input('angulo formado? ')
             if pergunta1 == '30':
-                a = ca / cos30
+                a = ca / valores[3]['cos30']
                 print(f'O valor de a é {a:.2f}')
             elif pergunta1 == '45':
-                a = ca / cos45
+                a = ca / valores[4]['cos45']
                 print(f'O valor de a é {a:.2f}')
             elif pergunta1 == '60':
-                a = ca / cos60
+                a = ca / valores[5]['cos60']
                 print(f'O valor de a é {a:.2f}')
     
     pergunta0 = input('Mais uma vez y/n? ')
